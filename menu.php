cbf94860ba40a7daeab59dfa5d4426d6424a6c2a
Cauã R. Pereira <7aauac@gmail.com>
2023-06-15 17:57:15 -0300

    feito menu dinâmico

--- menu.php
+++ menu.php
@@ -1,4 +1,23 @@
-    <!-- BEGIN HEADER -->
+<?php
+
+$array_menu = [
+	'Dashboard',
+	'Cadastro' => [
+		'Cliente',
+		'Fornecedor',
+		'Perfil de acesso',
+		'Produtos',
+		'Usuário',
+	],
+	'Relatório' => [
+		'Cliente',
+		'Faturamento',
+		'Produtos',
+	],
+];
+
+?>
+		<!-- BEGIN HEADER -->
     <div class="header navbar navbar-inverse navbar-fixed-top">
       <!-- BEGIN TOP NAVIGATION BAR -->
       <div class="header-inner">
@@ -444,74 +463,68 @@
     <!-- END HEADER -->
     <div class="clearfix"></div>
 		<!-- BEGIN CONTAINER -->
-<div class="page-container">
-  <!-- BEGIN SIDEBAR -->
-  <div class="page-sidebar-wrapper">
-    <div class="page-sidebar navbar-collapse collapse">
-      <!-- BEGIN SIDEBAR MENU -->
-      <ul class="page-sidebar-menu">
-        <li class="sidebar-toggler-wrapper">
-          <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-          <div class="sidebar-toggler hidden-phone"></div>
-          <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-        </li>
-        <li class="sidebar-search-wrapper">
-          <!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
-          <form class="sidebar-search" action="extra_search.html" method="POST">
-            <div class="form-container">
-              <div class="input-box">
-                <a href="javascript:;" class="remove"></a>
-                <input type="text" placeholder="Search..." />
-                <input type="button" class="submit" value=" " />
-              </div>
-            </div>
-          </form>
-          <!-- END RESPONSIVE QUICK SEARCH FORM -->
-        </li>
-        <li class="start active">
-          <a href="index.html">
-            <i class="fa fa-home"></i>
-            <span class="title"> Dashboard </span>
-            <span class="selected"> </span>
-          </a>
-        </li>
-        <!--Cliente-->
-        <li class="">
-          <a href="javascript:;">
-            <i class="fa fa-file-text"></i>
-            <span class="title"> Cadastro </span>
-            <span class="arrow"> </span>
-          </a>
-          <ul class="sub-menu">
-            <li>
-              <a href="#">Cliente</a>
-            </li>
-            <li>
-              <a href="#">Fornecedor</a>
-            </li>
-            <li>
-              <a href="#">Usuário</a>
-            </li>
-          </ul>
-        </li>
-        <!--Relatorio-->
-        <li class="">
-          <a href="javascript:;">
-            <i class="fa fa-bar-chart-o"></i>
-            <span class="title"> Relatório </span>
-            <span class="arrow"> </span>
-          </a>
-          <ul class="sub-menu">
-            <li>
-              <a href="#">Cliente</a>
-            </li>
-            <li>
-              <a href="#">Faturamento</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <!-- END SIDEBAR MENU -->
-    </div>
-  </div>
-  <!-- END SIDEBAR -->
+		<div class="page-container">
+			<!-- BEGIN SIDEBAR -->
+			<div class="page-sidebar-wrapper">
+				<div class="page-sidebar navbar-collapse collapse">
+					<!-- BEGIN SIDEBAR MENU -->
+					<ul class="page-sidebar-menu">
+						<li class="sidebar-toggler-wrapper">
+							<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+							<div class="sidebar-toggler hidden-phone"></div>
+							<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+						</li>
+						<li class="sidebar-search-wrapper">
+							<!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
+							<form class="sidebar-search" action="extra_search.html" method="POST">
+								<div class="form-container">
+									<div class="input-box">
+										<a href="javascript:;" class="remove"></a>
+										<input type="text" placeholder="Search..." />
+										<input type="button" class="submit" value=" " />
+									</div>
+								</div>
+							</form>
+							<!-- END RESPONSIVE QUICK SEARCH FORM -->
+						</li>
+						<?php
+						foreach ($array_menu as $key => $value):
+							if (!is_array($value)):
+								?>
+								<li class="start active">
+									<a href="index.html">
+										<i class="fa fa-home"></i>
+										<span class="title"> <?= $value ?> </span>
+										<span class="selected"> </span>
+									</a>
+								</li>
+								<?php
+							else:
+								?>
+								<li class="">
+									<a href="javascript:;">
+										<i class="<?php echo ($key == 'Cadastro') ? 'fa fa-file-text' : 'fa fa-bar-chart-o' ?>"></i>
+										<span class="title"> <?= $key ?> </span>
+										<span class="arrow"> </span>
+									</a>
+									<ul class="sub-menu">
+										<?php
+										foreach ($value as $sub_item):
+											?>
+											<li>
+												<a href="#"><?= $sub_item ?></a>
+											</li>
+											<?php
+										endforeach;
+										?>
+									</ul>
+								</li>
+								<?php
+							endif;
+						endforeach;
+						?>
+					</ul>
+					<!-- END SIDEBAR MENU -->
+				</div>
+			</div>
+			<!-- END SIDEBAR -->
